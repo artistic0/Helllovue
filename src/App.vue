173897538ff039c1e36b334fbe113e7d1fd29eaa
Akshay Singh <cu.18bcs1368@gmail.com>
2021-10-29 11:32:34 +0530

    First mini project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Hello name="Akshay"/>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+export default {
+  name : 'App',
+  components : {
+    Hello
+    }
+}
+</script>
+
---
+++ src/components/Hello.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hello {{name}}
+  </div>
+</template>
+<script>
+ export default {
+   name : 'Hello' ,
+   props : {
+     name: String
+   }
+ }
+ </script>
